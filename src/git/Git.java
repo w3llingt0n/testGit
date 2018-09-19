a7ac313ec65540e3acc4a28e72e105496f9fe3fa
Wellington Cardoso <wellingtonifnmg@gmail.com>
2018-09-19 20:30:17 -0300

    Messagem no main.

--- src/git/Git.java
+++ src/git/Git.java
@@ -15,7 +15,9 @@ public class Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("adwadd");
+        
     }
     
 }
